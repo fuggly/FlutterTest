fe218b2d283aff69b1bf7d7bb22cfffdc32898df
Roger Misteli <fuggly@mac.com>
2022-09-27 13:56:15 +0200

    .

--- FlutterTest/ViewController.swift
+++ FlutterTest/ViewController.swift
@@ -6,14 +6,30 @@
 //
 
 import UIKit
+import Flutter
+import FlutterPluginRegistrant
 
 class ViewController: UIViewController {
+
+  lazy var flutterEngines = FlutterEngineGroup(name: "deepId-ident", project: nil)
+  
+  lazy var flutterEngine: FlutterEngine = {
+    let result = flutterEngines.makeEngine(with: nil)
+    let thing = result.run(withEntrypoint: nil, initialRoute: "/idBack_document_scan")
+    print(thing)
+    return result
+  }()
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
   }
 
+  @IBAction
+  func bla() {
+    let flutterViewController = FlutterViewController(engine: self.flutterEngine, nibName: nil, bundle: nil)
+    self.present(flutterViewController, animated: false, completion: nil)
 
+  }
 }
 
